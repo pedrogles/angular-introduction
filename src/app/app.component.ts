e086a08784fad3be40fae927a879e5f2b2ddeae1
Pedro Gabriel <pedrogabriellima@gmail.com>
2024-10-21 10:33:42 -0300

    Interpolação de testo e event binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Component01Component } from "./component01/component01.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Component01Component],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-basico';
 }
---
+++ src/app/component01/component01.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-component01',
+  standalone: true,
+  imports: [],
+  templateUrl: './component01.component.html',
+  styleUrl: './component01.component.css'
+})
+export class Component01Component {
+  nome:string = "Pedro";
+  idade:number = 27;
+  mensagem() {
+    alert('Hello World');
+  }
+}
