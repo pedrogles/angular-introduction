6a3033dcd1f90dc0f51fef12a7d1ed38775880a0
Pedro Gabriel <pedrogabriellima@gmail.com>
2024-10-21 11:07:40 -0300

    *ngSwitch

---
+++ src/app/component05/component05.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-component05',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './component05.component.html',
+  styleUrl: './component05.component.css'
+})
+export class Component05Component {
+  linguagem:string = 'CSS';
+}
