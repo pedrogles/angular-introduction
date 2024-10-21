7f80a97788283b8cbf453418f095419ed8f00368
Pedro Gabriel <pedrogabriellima@gmail.com>
2024-10-21 10:34:44 -0300

    Property binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Component01Component } from "./component01/component01.component";
+import { Component02Component } from "./component02/component02.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, Component01Component],
+  imports: [RouterOutlet, Component01Component, Component02Component],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
