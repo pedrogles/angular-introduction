9ce6a3b437701b0be3a0168aa9f0ab20727ea57c
Pedro Gabriel <pedrogabriellima@gmail.com>
2024-10-21 15:06:59 -0300

    control flow condicional

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Component01Component } from "./component01/component01.component";
-import { Component02Component } from "./component02/component02.component";
-import { Component03Component } from "./component03/component03.component";
-import { Component04Component } from "./component04/component04.component";
-import { Component05Component } from "./component05/component05.component";
-import { Component06Component } from "./component06/component06.component";
+import { Component07Component } from "./component07/component07.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, Component01Component, Component02Component, Component03Component, Component04Component, Component05Component, Component06Component],
+  imports: [RouterOutlet, Component07Component],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/component07/component07.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-component07',
+  standalone: true,
+  imports: [],
+  templateUrl: './component07.component.html',
+  styleUrl: './component07.component.css'
+})
+export class Component07Component {
+  media:number = 6;
+}
