a9a17e39c61669a6845ae604251c9465c7d20e48
Pedro Gabriel <pedrogabriellima@gmail.com>
2024-10-22 07:52:09 -0300

    FormsModule

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Component08Component } from "./component08/component08.component";
+import { Component09Component } from "./component09/component09.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, Component08Component],
+  imports: [RouterOutlet, Component09Component],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
